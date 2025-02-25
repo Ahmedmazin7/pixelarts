043b157fb1d2e69e856579fbb6f24724be65258e
Ahmedmazin7 <maazin019@gmail.com>
2025-02-25 16:55:56 +0500

    added form functions

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+
 const toggleBtnOpen = document.querySelector('.toggle_btn i');
 const navBlock = document.querySelector('.nav_block');
 const toggleBtn = document.querySelector('.toggle_btn')
@@ -13,3 +14,13 @@ toggleBtn.addEventListener('click', () => {
   ?' fa-solid fa-xmark'
   : ' fa-solid fa-bars'
 });
+
+// form functions
+const submit = document.querySelector('.submit-btn');
+
+submit.addEventListener('click', () => {
+  const email = document.querySelector('.js-email').value;
+  const name = document.querySelector('.js-name').value;
+  const message = document.querySelector('.js-message').value;
+   email + name != "" ? console.log(email, name): alert("enter details");
+})
